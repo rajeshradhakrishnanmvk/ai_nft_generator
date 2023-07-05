a33c44f71944d1cc35c139e1692e153a8c5a753a
rajeshradhakrishnanmvk <rajeshmvk@gmail.com>
2023-07-05 19:08:08 +0530

    added market place

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -1,43 +1,92 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.0;
+pragma solidity 0.8.18;
 
-import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Royalty.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract NFT is ERC721URIStorage {
+contract NFT is
+    ERC721,
+    ERC721Enumerable,
+    ERC721URIStorage,
+    ERC721Royalty,
+    Ownable
+{
+    using Strings for uint256;
     using Counters for Counters.Counter;
+
     Counters.Counter private _tokenIds;
 
-    address public owner;
-    uint256 public cost;
+    string public metadataURI;
+
+    address public artist;
+    uint96 public royaltyFee;
 
     constructor(
         string memory _name,
         string memory _symbol,
-        uint256 _cost
+        string memory _metadataURI,
+        address _artist,
+        uint96 _royaltyFee
     ) ERC721(_name, _symbol) {
-        owner = msg.sender;
-        cost = _cost;
+        metadataURI = _metadataURI;
+        artist = _artist;
+        royaltyFee = _royaltyFee;
     }
 
-    function mint(string memory tokenURI) public payable {
-        require(msg.value >= cost);
+    function mint() public {
+        uint256 tokenId = _tokenIds.current();
+
+        _safeMint(msg.sender, tokenId);
+        _setTokenURI(
+            tokenId,
+            string(abi.encodePacked(metadataURI, tokenId.toString(), ".json"))
+        );
+        _setTokenRoyalty(tokenId, artist, royaltyFee);
 
         _tokenIds.increment();
-
-        uint256 newItemId = _tokenIds.current();
-        _mint(msg.sender, newItemId);
-        _setTokenURI(newItemId, tokenURI);
     }
 
-    function totalSupply() public view returns (uint256) {
-        return _tokenIds.current();
+    function setRoyaltyFee(uint96 _royaltyFee) public onlyOwner {
+        // This just updates the royaltyFee state variable, you can alternatively pass in a
+        // _tokenId parameter and call _setTokenRoyalty() to update an already minted token
+        royaltyFee = _royaltyFee;
     }
 
-    function withdraw() public {
-        require(msg.sender == owner);
-        (bool success, ) = owner.call{value: address(this).balance}("");
-        require(success);
+    // The following functions are overrides required by Solidity.
+
+    function _burn(
+        uint256 tokenId
+    ) internal virtual override(ERC721, ERC721URIStorage, ERC721Royalty) {
+        super._burn(tokenId);
+        _resetTokenRoyalty(tokenId);
+    }
+
+    function tokenURI(
+        uint256 tokenId
+    ) public view override(ERC721, ERC721URIStorage) returns (string memory) {
+        return super.tokenURI(tokenId);
+    }
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId,
+        uint256 batchSize
+    ) internal override(ERC721, ERC721Enumerable) {
+        super._beforeTokenTransfer(from, to, tokenId, batchSize);
+    }
+
+    function supportsInterface(
+        bytes4 interfaceId
+    )
+        public
+        view
+        override(ERC721, ERC721Enumerable, ERC721Royalty)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
     }
 }
--- test/NFT.js
+++ test/NFT.js
@@ -1,79 +1,66 @@
-const { expect } = require('chai');
-const { ethers } = require('hardhat');
+const { expect } = require("chai")
+const { ethers } = require("hardhat")
 
-const tokens = (n) => {
-  return ethers.utils.parseUnits(n.toString(), 'ether')
-}
-
-describe('NFT', () => {
-  let deployer, minter
-  let nft
-
+describe("NFT", () => {
   const NAME = "AI Generated NFT"
   const SYMBOL = "AINFT"
-  const COST = tokens(1) // 1 ETH
-  const URL = "https://ipfs.io/ipfs/bafyreid4an6ng6e6hok56l565eivozra3373bo6funw3p5mhq5oonew6u4/metadata.json"
+  const METADATA_URL = "https://ipfs.io/ipfs/bafyreigdrzlwrga2e6uf3daxwns573eejo7rifzxed4w5iaeszruchsjoy/metadata.json"
+  const ROYALTY_FEE = 500 // 5%
+
+  let deployer, artist, minter
+  let nft
 
   beforeEach(async () => {
-    // Setup accounts
-    [deployer, minter] = await ethers.getSigners()
+    [deployer, artist, minter] = await ethers.getSigners()
 
-    // Deploy Real Estate
-    const NFT = await ethers.getContractFactory('NFT')
-    nft = await NFT.deploy(NAME, SYMBOL, COST)
-
-    // Mint 
-    const transaction = await nft.connect(minter).mint(URL, { value: COST })
-    await transaction.wait()
+    const NFT = await ethers.getContractFactory("NFT")
+    nft = await NFT.deploy(NAME, SYMBOL, METADATA_URL, artist.address, ROYALTY_FEE)
   })
 
   describe('Deployment', () => {
-    it('Returns owner', async () => {
-      const result = await nft.owner()
-      expect(result).to.be.equal(deployer.address)
+    it('Returns the name', async () => {
+      const result = await nft.name()
+      expect(result).to.equal(NAME)
     })
 
-    it('Returns cost', async () => {
-      const result = await nft.cost()
-      expect(result).to.be.equal(COST)
-    })
-  })
-
-  describe('Minting', () => {
-    it('Returns owner', async () => {
-      const result = await nft.ownerOf("1")
-      expect(result).to.be.equal(minter.address)
+    it('Returns the symbol', async () => {
+      const result = await nft.symbol()
+      expect(result).to.equal(SYMBOL)
     })
 
-    it('Returns URI', async () => {
-      const result = await nft.tokenURI("1")
-      expect(result).to.be.equal(URL)
+    it('Returns the royalty fee', async () => {
+      const result = await nft.royaltyFee()
+      expect(result).to.equal(ROYALTY_FEE)
     })
 
-    it('Updates total supply', async () => {
-      const result = await nft.totalSupply()
-      expect(result).to.be.equal("1")
+    it('Returns the artist', async () => {
+      const result = await nft.artist()
+      expect(result).to.equal(artist.address)
     })
   })
 
-  describe('Withdrawing', () => {
-    let balanceBefore
-
+  describe('Minting & Royalty Info', () => {
     beforeEach(async () => {
-      balanceBefore = await ethers.provider.getBalance(deployer.address)
-
-      const transaction = await nft.connect(deployer).withdraw()
+      const transaction = await nft.connect(minter).mint()
       await transaction.wait()
     })
 
-    it('Updates the owner balance', async () => {
-      const result = await ethers.provider.getBalance(deployer.address)
-      expect(result).to.be.greaterThan(balanceBefore)
+    it('Sets royalty info', async () => {
+      const COST = ethers.utils.parseUnits("1", "ether") // Let's assume we sell NFT for 1 ETH
+
+      const result = await nft.royaltyInfo(0, COST)
+      expect(result[0]).to.equal(artist.address)
+      expect(result[1]).to.equal("50000000000000000") // We expect the royalty to be 0.05 ETH (5%)
     })
 
-    it('Updates the contract balance', async () => {
-      const result = await ethers.provider.getBalance(nft.address)
-      expect(result).to.equal(0)
+    it('Updates totalSupply', async () => {
+      const result = await nft.totalSupply()
+      expect(result).to.equal(1)
+    })
+
+    it('Updates ownership', async () => {
+      const result = await nft.ownerOf(0)
+      expect(result).to.equal(minter.address)
     })
   })
 })
